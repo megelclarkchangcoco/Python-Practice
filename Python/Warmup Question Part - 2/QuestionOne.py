b7b2958bfc1838c4ac1465e5c6f5c97ee048f9da
WhosyourDaddy <123862183+megelclarkchangcoco@users.noreply.github.com>
2024-04-28 03:52:00 +0800

    Create QuestionOne.py

---
+++ Python/Warmup Question Part - 2/QuestionOne.py
@@ -0,0 +1,18 @@
+# 1. Control Loop Question:
+#
+# Question: Write a Python program that takes an integer input n and prints all the numbers from 1 to n using a for loop.
+#
+# Sample Input: 5
+#
+# Sample Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+# n = 5
+#
+# for i in range(1, n + 1):
+#     print(i)
